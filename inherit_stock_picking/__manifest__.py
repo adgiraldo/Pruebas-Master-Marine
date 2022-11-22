e95da694306d4b740d20a41fd3104e283cc7f324
fabian-odoo <facos8@hotmail.com>
2022-11-22 08:43:26 -0500

    add new app

---
+++ inherit_stock_picking/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name':'stock_picking_aux_report',
+    'depends': ['stock'],
+    'description': """Aux Module for Transfer Operations""",
+    'author': '',
+    'category': '',
+    'data': [
+        #aqui la url de las vistas
+        'views/stock_picking_aux_report.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': True,
+    'license': 'Other proprietary',
+}
